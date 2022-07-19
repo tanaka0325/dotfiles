20f05956fc78bd40d9df585938baf9c9ec9d0a89
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2022-07-19 19:41:51 +0900

    Add hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,10 @@
+hs.hotkey.bind({"ctrl", "shift"}, "t", function()
+    local kitty = hs.application.get("kitty")
+    if kitty == nil then
+        hs.application.launchOrFocus("/Applications/kitty.app")
+    elseif kitty:isFrontmost() then
+        kitty:hide()
+    else
+        hs.application.launchOrFocus("/Applications/kitty.app")
+    end
+end)
