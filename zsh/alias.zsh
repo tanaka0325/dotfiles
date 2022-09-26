01cfa39d149d4020feb5819776f3e01943aa426f
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2022-09-26 11:49:58 +0900

    Add rgr alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,6 +2,10 @@
 alias np='hugo new post/$(date "+%Y%m%d%H%M%S").md'
 alias rm="rm -i"
 alias ls="ls -G"
+
+# replace using rg, sd
+alias rgr='(){rg $1 -r $2 --files-with-matches | xargs -I{} sd $1 $2 {} }'
+
 if [ `uname -s` = 'Darwin' ]; then
     alias ls='ls -G'
     alias ll='ls -lhaG'
