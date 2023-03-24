16bd960d23dd05bc3df8912ad4b975d67ea72e70
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2023-03-24 03:02:21 +0000

    up

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,6 +2,7 @@
 alias np='hugo new post/$(date "+%Y%m%d%H%M%S").md'
 alias rm="rm -i"
 alias ls="ls -G"
+alias g="git"
 
 # replace using rg, sd
 alias rgr='(){rg $1 -r $2 --files-with-matches $3 | xargs -I{} sd $1 $2 $3 {} }'
